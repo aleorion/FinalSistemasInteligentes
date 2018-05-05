c78db2eea15d28ebfa5c72c072dcd23a3f9662dc
aleorion <aletmdec@hotmail.com>
2018-05-05 01:19:57 -0500

    corrected gen base class

--- Gen.java
+++ Gen.java
@@ -2,11 +2,12 @@ import java.util.ArrayList;
 
 public class Gen
 {
+    Materia m; // same as position on the Genoma!
     Profesor p;
     Horario h;
     Salon s;
 
-    public Gen(ArrayList<Profesor> profesores, Salon salones)
+    public Gen(ArrayList<Profesor> profesores, Salon salones, Materia m)
     {
         //Create a ramdom valid gen from profesor, horario, salon.
     }
